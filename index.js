fe7349edfa38ba0407dafe13158a26d20b521bac
emilejama <kalnasduobe@gmail.com>
2019-05-04 01:23:43 +0800

    database login test

---
+++ index.js
@@ -0,0 +1,43 @@
+//var mainText = document.getElementById("mainText");
+//var submitBtn = document.getElementById("submitBtn");
+
+//function submitClick() {
+	
+	//var firebaseRef = firebase.database().ref();
+	
+//	var messageText = mainText.value;
+	
+//	firebaseRef.push().set(messageText);
+	
+//}
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
+    
+  } else {
+    // No user is signed in.
+     document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block"; //can be initial insted of block
+    
+  }
+});
+
+
+function login(){
+	var userEmail = document.getElementById("email_field").value;
+	var userPass = document.getElementById("password_field").value;
+	
+firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  
+  window.alert("Error : " errorMessage);
+  
+  // ...
+});
+
+}
