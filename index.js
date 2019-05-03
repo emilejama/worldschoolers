9f7fb8409a9cabe1779f158a25d78a66449f48c7
emilejama <kalnasduobe@gmail.com>
2019-05-04 02:29:19 +0800

    login logout

--- index.js
+++ index.js
@@ -17,6 +17,16 @@ firebase.auth().onAuthStateChanged(function(user) {
     document.getElementById("user_div").style.display = "block";
     document.getElementById("login_div").style.display = "none";
     
+    var user = firebase.auth().currentUser;
+
+	if(user != null){
+		
+		var email_id =user.email;
+		
+		document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+		
+	}
+    
   } else {
     // No user is signed in.
      document.getElementById("user_div").style.display = "none";
@@ -30,14 +40,23 @@ function login(){
 	var userEmail = document.getElementById("email_field").value;
 	var userPass = document.getElementById("password_field").value;
 	
-firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  
-  window.alert("Error : " errorMessage);
-  
-  // ...
-});
+	firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  
+	  window.alert("Error : " + errorMessage);
+	  
+	  // ...
+	});
 
 }
+
+	function logout(){
+		
+		firebase.auth().signOut();
+
+		
+	}
+
+
