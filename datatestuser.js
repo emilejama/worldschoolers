32648d5f5adde3c0e511b1243cbfd8f261063e30
emilejama <kalnasduobe@gmail.com>
2019-05-04 17:20:06 +0800

    user data base test

---
+++ datatestuser.js
@@ -0,0 +1,20 @@
+
+
+
+var mainText = document.getElementById("mainText");
+var submitBtn = document.getElementById("submitBtn");
+
+function submitClick() {
+	
+	var firebaseRef = firebase.database().ref();
+	
+	//firebaseRef.child("Text").set("Some Value");
+	
+	
+}
+	
+		//var messageText = mainText.value;
+	
+	//firebaseRef.push().set(messageText);
+	
+//}
