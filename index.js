ec1903082059050843dfb751353280962fc8bac5
emilejama <emile.norku@gmail.com>
2019-05-11 12:56:03 +0100

    create account

--- index.js
+++ index.js
@@ -22,8 +22,9 @@ firebase.auth().onAuthStateChanged(function(user) {
 	if(user != null){
 		
 		var email_id =user.email;
-		
-		document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+		var email_verified = user.emailVerified;
+		document.getElementById("user_para").innerHTML = "Welcome : " + email_id + 
+															"<br/> Verified : " + email_verified;
 		
 	}
     
@@ -52,6 +53,24 @@ function login(){
 
 }
 
+	function create_account() {
+		
+		var userEmail = document.getElementById("email_field").value;
+		var userPass = document.getElementById("password_field").value;
+		
+		
+	firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  
+	  window.alert("Error : " + errorMessage);
+	  
+	  // ...
+	});
+		
+	}
+
 	function logout(){
 		
 		firebase.auth().signOut();
@@ -59,4 +78,17 @@ function login(){
 		
 	}
 
+	function send_verification() {
+		
+		var user = firebase.auth().currentUser;
 
+user.sendEmailVerification().then(function() {
+  // Email sent.
+  window.alert("Verification sent")
+}).catch(function(error) {
+  // An error happened.
+   window.alert(" Error : " + error.message);
+});
+
+		
+	}
