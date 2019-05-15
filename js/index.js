19a34be8485d77ea899dff65ba6e1163a6079ff2
emilejama <emile.norku@gmail.com>
2019-05-16 02:19:51 +0300

    myprofile padarem su adomu

---
+++ js/index.js
@@ -0,0 +1,197 @@
+//var mainText = document.getElementById("mainText");
+//var submitBtn = document.getElementById("submitBtn");
+
+//function submitClick() {
+	
+	//var firebaseRef = firebase.database().ref();
+	
+//	var messageText = mainText.value;
+	
+//	firebaseRef.push().set(messageText);
+	
+//}
+
+var uid = null;
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
+    
+    var user = firebase.auth().currentUser;
+
+	if(user != null){
+		
+		var email_id =user.email;
+		var email_verified = user.emailVerified;
+		
+	if(email_verified){
+		document.getElementById("verify_btn").style.display = "none";
+	} else {
+		
+		document.getElementById("verify_btn").style.display = "block";
+	}
+		
+		document.getElementById("user_para").innerHTML = "Welcome : " + email_id + 
+															"<br/> Verified : " + email_verified;
+		
+	}
+    
+  } else {
+    // No user is signed in.
+     document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block"; //can be initial insted of block
+    
+  }
+});
+
+
+function login(){
+	var userEmail = document.getElementById("email_field").value;
+	var userPass = document.getElementById("password_field").value;
+	
+	firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  
+	  window.alert("Error : " + errorMessage);
+	  
+	  // ...
+	});
+
+}
+
+firebase.auth().onAuthStateChanged(user => {
+  if(user) {
+	  console.log(user);
+				var user = firebase.auth().currentUser;
+		var name, email, photoUrl, emailVerified;
+
+		if (user != null) {
+//		  name = user.displayName;
+		  email = user.email;
+	//	  photoUrl = user.photoURL;
+	//	  emailVerified = user.emailVerified;
+		  uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
+						   // this value to authenticate with your backend server, if
+						   // you have one. Use User.getToken() instead.
+						   console.log('aaaa',email)
+						   
+				
+		document.getElementById("profilius").innerHTML = myprofiletemplate;
+		console.log('spelioju');
+		document.getElementById("profilius").style.display = "block";
+		console.log('spelioju');
+			   
+//console.log('kviechia,',uid);
+//writeUserData(uid, "name", "email", "imageUrl");
+		}
+    //window.location = 'index.html'; //After successful login, user will be redirected to home.html
+  }
+});
+
+function create_account() {
+	
+	var userEmail = document.getElementById("email_field").value;
+	var userPass = document.getElementById("password_field").value;
+		
+		
+	firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  
+	  window.alert("Error : " + errorMessage);
+	  
+	  // ...
+	}).then(function(){
+		document.getElementById("profilius").style.display = "block";
+		console.log('spelioju');
+	});	
+}
+	
+
+// re-autentification nesigauna, jei antra karta noriu keist passworda laiskas buna su nuoroda, kuri sako, kad jau panaudota.
+
+function forgot_password(){
+
+
+	var userEmail = document.getElementById("email_field").value;
+	var userPass = document.getElementById("password_field").value;
+	
+	var auth = firebase.auth();
+	var emailAddress = document.getElementById("email_field").value;
+
+	auth.sendPasswordResetEmail(emailAddress).then(function() {
+	// Email sent.
+		window.alert('Email Sent');
+	}).catch(function(error) {
+	// An error happened.
+		var errorCode = error.code;
+		var errorMessage = error.message;
+		if(errorCode =="auth/invalid-email") {
+			alert(errorMessage);
+		} else if (errorCode == "auth/user-not-found") {
+			alert(errorMessage);
+			
+		}
+		console.log(error);
+	});
+
+	
+}
+
+
+function logout(){
+	
+	firebase.auth().signOut();
+
+	document.getElementById("profilius").innerHTML = "tests";
+		console.log('spelioju');
+		document.getElementById("profilius").style.display = "none";
+		console.log('spelioju');
+}
+
+function send_verification() {
+	
+	var user = firebase.auth().currentUser;
+
+	user.sendEmailVerification().then(function() {
+	  // Email sent.
+	  window.alert("Verification sent")
+	}).catch(function(error) {
+	  // An error happened.
+	   window.alert(" Error : " + error.message);
+	});
+
+	
+}
+	
+/*	
+	  console.log('stR GGalen',firebase.auth());
+
+var userId = firebase.auth().currentUser.uid;
+var sttus = firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
+  var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+  //console.log('GGalen',username);
+});
+ console.log('stR GGalen',sttus);
+
+*/
+
+
+///////////////BANDAU GAUT USERIO PROFILI
+
+
+// Get a reference to the database service
+var database = firebase.database();
+
+function writeUserData(userId, name, email, imageUrl) {
+  firebase.database().ref('users/' + userId).set({
+    address: name,
+    email: email,
+    picture : imageUrl
+  });
+}
---
+++ js/templates/profile.js
@@ -0,0 +1,90 @@
+
+var myprofiletemplate = `
+
+
+	
+		
+ <div class="search-container">
+    <form action="/action_page.php">
+      <input type="text" placeholder="Search.." name="search">
+      <button type="submit">Search &#128269;</button>
+    </form>
+  </div>
+
+
+
+	<!--<div class="card">
+  <img src="/w3images/team2.jpg" alt="John" style="width:100%">
+  <h1>John Doe</h1>
+  <p class="title">CEO & Founder, Example</p>
+  <p>Harvard University</p>
+  <div  class="userprofile-icons"style="margin: 24px 0;">
+    <a href="#"><i class="fa fa-dribbble"></i></a> 
+    <a href="#"><i class="fa fa-twitter"></i></a>  
+    <a href="#"><i class="fa fa-linkedin"></i></a>  
+    <a href="#"><i class="fa fa-facebook"></i></a> 
+  </div>
+  <p><button class="userprofile-btn">Contact</button></p>
+</div> -->
+
+
+<div class="user-profile" >
+	           <img src="img/europe.jpg"  class="responsive europe user-profile-img" href="region/europe.htm" > 
+               <div class="user-profile-info" >
+					<h2>User Name</h2>
+					<div class="location-markers">
+									<div class="location"><!-- laikas kada pridetas markeris-->(marker icon)las palmas de gran canaria, 28 Oct 2019</div>
+									 <div id="map" style=" width:30%;
+									 height:400px;
+									"></div>
+									<div class="location-history-dropbar ">
+												<p>Location history: </p>
+												<p>recent places: </p>
+									<div class="all-locations"> 
+												<ul>
+														<li> Vilnius, 20 sep 2019</li>
+														<li> London, 20 sep 2019</li>
+														<li> italy, 20 sep 2019</li>
+												</ul>
+									</div>
+									<button>showmore/less</button>
+									</div>
+					</div>
+					
+					<div class="description">
+							<p>Description:<p>
+							<p>Lorem ipsum dolor sit amet, consectetur adipi iki 400 simboliu</p>
+					</div>
+			</div>				
+					<div>
+						<a href="#"><i class="fa fa-twitter"></i></a>
+						<a href="#"><i class="fa fa-linkedin"></i></a>
+						<a href="#"><i class="fa fa-facebook"></i></a>
+								
+					</div>
+
+
+	
+</div>
+<div>
+	<button><a href="edit-myprofile.html">Edit</a></button>
+</div>
+
+
+
+
+<script>
+
+function myMap() {
+var mapProp= {
+  center:new google.maps.LatLng(51.508742,-0.120850),
+  //q: "lietuva",
+  zoom:5,
+};
+var map = new google.maps.Map(document.getElementById("map"),mapProp);
+}
+</script>
+
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDUPsVJQgLHBRrdpL94Bn3RI7ZnHYgVg68 &callback=myMap"></script>
+
+`;
